fa6f415b5857ccbc5ad068588733fd466bf370e0
Syun <syunk38@gmail.com>
2016-06-27 01:03:11 +0900

    metaストリームを作る

--- src/index.js
+++ src/index.js
@@ -4,6 +4,26 @@ $(() => {
     let btn = document.querySelector('#btn');
     let btnStream = Rx.Observable.fromEvent(btn, 'click');
 
+    let requestStream = Rx.Observable.returnValue('https://api.github.com/users');
+
+    requestStream.subscribe(requestUrl => {
+      let responseStream = Rx.Observable.create(observer => {
+        $.getJSON(requestUrl)
+          .done(response => { observer.onNext(response); })
+          .fail((jqXHR, status, error) => { observer.onError(error); })
+          .always(() => { observer.onCompleted(); });
+      });
+    });
+
+    let responseMetaStream = requestStream
+      .map(requestUrl =>{
+        return Rx.Observable.fromPromise($.getJSON(requestUrl));
+      });
+
+    responseStream.subscribe((response => {
+      console.log(response);
+    }));
+
     btnStream.subscribe(
       x => {
         $('ul').append(`<li>Next: ${x.x}</li>`);
