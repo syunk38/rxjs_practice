7b025f027cf15e9695dc98daee174fb0653f0082
Syun <syunk38@gmail.com>
2016-06-30 00:45:04 +0900

    3人のSuggestを行う

--- src/index.js
+++ src/index.js
@@ -7,21 +7,61 @@ $(() => {
 
   const requestStream = refreshBtnStream
     .startWith('start click')
-    .do(x => {console.log(x)})
     .map(event => { return `https://api.github.com/users?since=${event.x + event.y}`})
+
 
 
   const responseStream = requestStream
     .flatMap(requestUrl =>{
       return Rx.Observable
         .fromPromise($.getJSON(requestUrl))
-        .flatMap(responses => {
-          return Rx.Observable.from(responses);
-        })
     });
 
-  responseStream.subscribe(data => {
-    $('ul').append(`<li><img src="${data.avatar_url}" height="150" width="150"/></li>`);
-    $('ul').append(`<li>${data.login}</li>`);
+  const suggestion1Stream = responseStream
+    .map(listUsers => {
+      return listUsers[Math.floor(Math.random() * listUsers.length)]
+    })
+    .merge(refreshBtnStream.map(() => { return null }));
+
+  const suggestion2Stream = responseStream
+    .map(listUsers => {
+      return listUsers[Math.floor(Math.random() * listUsers.length)]
+    })
+    .merge(refreshBtnStream.map(() => { return null }));
+
+  const suggestion3Stream = responseStream
+    .map(listUsers => {
+      return listUsers[Math.floor(Math.random() * listUsers.length)]
+    })
+    .merge(refreshBtnStream.map(() => { return null }));
+
+  suggestion1Stream.subscribe(suggestion => {
+    if (suggestion === null){
+      $('ul li:first').remove();
+    }
+    else{
+      $('ul').append(`<li><img src="${suggestion.avatar_url}" height="150" width="150"/></li>`);
+      // $('ul').append(`<li>${suggestion.login}</li>`);
+    }
+  });
+
+  suggestion2Stream.subscribe(suggestion => {
+    if (suggestion === null){
+      $('ul li:nth-child(2)').remove();
+    }
+    else{
+      $('ul').append(`<li><img src="${suggestion.avatar_url}" height="150" width="150"/></li>`);
+      // $('ul').append(`<li>${suggestion.login}</li>`);
+    }
+  });
+
+  suggestion3Stream.subscribe(suggestion => {
+    if (suggestion === null){
+      $('ul li:last').remove();
+    }
+    else{
+      $('ul').append(`<li><img src="${suggestion.avatar_url}" height="150" width="150"/></li>`);
+      // $('ul').append(`<li>${suggestion.login}</li>`);
+    }
   });
 });
